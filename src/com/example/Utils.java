c6e546cb9ee61677ddfbc77cab3ec751acee4912
David Beaumont <dbeaumont@google.com>
2020-12-07 13:45:11 +0100

    snapshot

--- src/com/example/Utils.java
+++ src/com/example/Utils.java
@@ -2,7 +2,6 @@ package com.example;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -16,7 +15,7 @@ public final class Utils {
     checkArgument(m.matches(), "no match for '%s' using: %s", p, s);
     List<String> groups = new ArrayList<>();
     for (int n = 1; n <= m.groupCount(); n++) {
-      groups.add(m.group(1));
+      groups.add(m.group(n));
     }
     fn.accept(groups);
   }
