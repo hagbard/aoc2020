862eaab8bdbb87262fee176386533ae975cc033f
David Beaumont <dbeaumont@google.com>
2020-12-12 23:04:40 +0100

    snapshot

---
+++ src/com/example/day20/Main.java
@@ -0,0 +1,11 @@
+package com.example.day20;
+
+import com.example.Input;
+import java.util.stream.Stream;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Stream<String> input = Input.getLines(Main.class);
+  }
+}
