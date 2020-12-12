4a3416f6f4be790f0ecf980bccb554c692e13fb1
David Beaumont <dbeaumont@google.com>
2020-12-12 22:28:24 +0100

    snapshot

--- src/com/example/day12/Main.java
+++ src/com/example/day12/Main.java
@@ -3,7 +3,6 @@ package com.example.day12;
 import static com.example.Vec2.vec;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.lang.Integer.parseUnsignedInt;
-import static java.lang.Math.floorMod;
 
 import com.example.Input;
 import com.example.Vec2;
@@ -13,32 +12,36 @@ import com.google.common.collect.ImmutableMap;
 // Dist = 2280
 // Dist = 38693
 public class Main {
-  private static final ImmutableMap<Integer, Vec2> DIRS =
-      ImmutableMap.of(0, vec(1, 0), 90, vec(0, -1), 180, vec(-1, 0), 270, vec(0, 1));
+  private static final ImmutableList<Vec2> DIRS =
+      ImmutableList.of(vec(1, 0), vec(0, -1), vec(-1, 0), vec(0, 1));
 
   private interface ShipOp {
     void apply(Ship s, int n);
   }
 
+  private static int angleToDir(int angle) {
+    return (angle / 90) & 0x3;
+  }
+
   private static final ImmutableMap<Character, ShipOp> FOO_OPS =
       ImmutableMap.<Character, ShipOp>builder()
           .put('E', (s, n) -> s.moveBy(DIRS.get(0).mul(n)))
-          .put('S', (s, n) -> s.moveBy(DIRS.get(90).mul(n)))
-          .put('W', (s, n) -> s.moveBy(DIRS.get(180).mul(n)))
-          .put('N', (s, n) -> s.moveBy(DIRS.get(270).mul(n)))
-          .put('L', (s, n) -> s.rotateShip(-n))
-          .put('R', (s, n) -> s.rotateShip(n))
+          .put('S', (s, n) -> s.moveBy(DIRS.get(1).mul(n)))
+          .put('W', (s, n) -> s.moveBy(DIRS.get(2).mul(n)))
+          .put('N', (s, n) -> s.moveBy(DIRS.get(3).mul(n)))
+          .put('L', (s, n) -> s.rotateShip(angleToDir(-n)))
+          .put('R', (s, n) -> s.rotateShip(angleToDir(n)))
           .put('F', Ship::moveForward)
           .build();
 
   private static final ImmutableMap<Character, ShipOp> BAR_OPS =
       ImmutableMap.<Character, ShipOp>builder()
           .put('E', (s, n) -> s.moveWaypoint(DIRS.get(0).mul(n)))
-          .put('S', (s, n) -> s.moveWaypoint(DIRS.get(90).mul(n)))
-          .put('W', (s, n) -> s.moveWaypoint(DIRS.get(180).mul(n)))
-          .put('N', (s, n) -> s.moveWaypoint(DIRS.get(270).mul(n)))
-          .put('L', (s, n) -> s.rotateWaypoint(-n))
-          .put('R', (s, n) -> s.rotateWaypoint(n))
+          .put('S', (s, n) -> s.moveWaypoint(DIRS.get(1).mul(n)))
+          .put('W', (s, n) -> s.moveWaypoint(DIRS.get(2).mul(n)))
+          .put('N', (s, n) -> s.moveWaypoint(DIRS.get(3).mul(n)))
+          .put('L', (s, n) -> s.rotateWaypoint(angleToDir(-n)))
+          .put('R', (s, n) -> s.rotateWaypoint(angleToDir(n)))
           .put('F', Ship::moveToWaypoint)
           .build();
 
@@ -49,9 +52,10 @@ public class Main {
   }
 
   private static class Ship {
+
     private final ImmutableMap<Character, ShipOp> ops;
     private Vec2 pos = vec(0, 0);
-    private int heading = 0;
+    private int headingDir = 0;  // [0..3]
     private Vec2 waypoint = vec(10, 1);
 
     Ship(ImmutableMap<Character, ShipOp> ops) {
@@ -63,26 +67,26 @@ public class Main {
     }
 
     void moveForward(int dist) {
-      moveBy(DIRS.get(heading).mul(dist));
+      moveBy(DIRS.get(headingDir).mul(dist));
     }
 
     void moveToWaypoint(int times) {
       moveBy(waypoint.mul(times));
     }
 
-    void rotateShip(int turn) {
-      this.heading = floorMod(heading + turn, 360);
+    void rotateShip(int dir) {
+      this.headingDir = (headingDir + dir) & 0x3;
     }
 
     void moveWaypoint(Vec2 adj) {
       waypoint = waypoint.add(adj);
     }
 
-    void rotateWaypoint(int turn) {
-      switch (floorMod(turn, 360)) {
-        case 90: waypoint = vec(waypoint.y, -waypoint.x); break;
-        case 180: waypoint = vec(-waypoint.x, -waypoint.y); break;
-        case 270: waypoint = vec(-waypoint.y, waypoint.x); break;
+    void rotateWaypoint(int dir) {
+      switch (dir) {
+        case 1: waypoint = vec(waypoint.y, -waypoint.x); break;
+        case 2: waypoint = vec(-waypoint.x, -waypoint.y); break;
+        case 3: waypoint = vec(-waypoint.y, waypoint.x); break;
       }
     }
 
