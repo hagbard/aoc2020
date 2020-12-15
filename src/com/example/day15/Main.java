f66be6a402b3f955c9942878e01c36cf7ef5d3f7
David Beaumont <dbeaumont@google.com>
2020-12-15 11:58:40 +0100

    snapshot

--- src/com/example/day15/Main.java
+++ src/com/example/day15/Main.java
@@ -1,11 +1,27 @@
 package com.example.day15;
 
-import com.example.Input;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class Main {
-
   public static void main(String[] args) {
-    Stream<String> input = Input.getLines(Main.class);
+    System.out.format("Answer = %d\n", getAnswer(2020, 9, 19, 1, 6, 0, 5, 4));
+    System.out.format("Answer = %d\n", getAnswer(30000000, 9, 19, 1, 6, 0, 5, 4));
+  }
+
+  private static int getAnswer(int endRound, int... inputs) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int n = 0; n < inputs.length; n++) {
+      map.put(inputs[n], n + 1);
+    }
+    int round = inputs.length;
+    int value = inputs[round - 1];
+    do {
+      Optional<Integer> lastIndex = Optional.ofNullable(map.put(value, round));
+      int lastRound = round++;
+      value = lastIndex.map(n -> lastRound - n).orElse(0);
+    } while (round < endRound);
+    return value;
   }
 }
